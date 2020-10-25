db3b096a571bff6fae55731c4142f6a9fdb3e3be
lamhan3012cmvn <laman3012@gmail.com>
2020-10-25 16:46:11 +0700

    ''

--- Demo/main.js
+++ Demo/main.js
@@ -1,136 +1,99 @@
-class demo{
-    constructor(){
-        this.canvas=null;
-        this.context=null;
-        this.arr=[];
-        this.color=[];
+class demo {
+    constructor() {
+        this.canvas = null;
+        this.context = null;
+        this.arr = [];
+        this.color = [];
         this.init();
     }
-    //Khởi tạo
-    init(){
-        this.canvas=document.createElement('canvas');
-        this.canvas.width=1200;
-        this.canvas.height=1000;
-        this.context=this.canvas.getContext('2d');
+    init() {
+        this.canvas = document.createElement('canvas');
+        this.canvas.width = 1000;
+        this.canvas.height = 1000;
+        this.context = this.canvas.getContext('2d');
         document.body.appendChild(this.canvas);
-        for(let i=0;i<100;i++)
-        {
+        for (let i = 0; i < 1000; i++) {
             this.arr.push(i);
-            this.color.push(3);//chưa sắp
+            this.color.push(0);
         }
-            
-        this.arr.sort(()=>{
-            return Math.random()-0.5;
+
+        this.arr.sort(() => {
+            return Math.random() - 0.5;
         })
-        this.mergeSort(this.arr,0,this.arr.length-1);
+        this.mergeSort(this.arr, 0, this.arr.length - 1);
         this.loop();
     }
-    //Sort
-    async mergeSort(arr,start,end){
-       
-        if(start<end)
-        {
-            const mid=Math.floor((start+end)/2);
-            await this.mergeSort(arr,start,mid);
-            // for(let i=mid+1;i<end+1;i++)
-            //     this.color[i]=1;//Màu vàng tách ra merge
-            await this.mergeSort(arr,mid+1,end);
-            await this.unMerge(arr,start,mid,end);
-            // for(let i=start;i<end+1;i++)
-            //     this.color[i]=0;//Màu đỏ đã unMerge
-            //this.color[start]=0;
-            //this.color[end]=0;
+    async mergeSort(arr, start, end) {
+        if (start < end) {
+            const mid = Math.floor((start + end) / 2);
+            await this.mergeSort(arr, start, mid);
+            await this.mergeSort(arr, mid + 1, end);
+            await this.unMerge(arr, start, mid, end);
         }
-      
+
     }
-    async unMerge(arr,start,mid,end){
-        this.color[start]=1;
-        this.color[end]=1;
-        let n1=mid-start+1;
-        let n2=end-mid;
-        const L=[];
-        const R=[];
-        for (let i = 0; i < n1; ++i) 
-        {
-            L.push(arr[start + i]);
+    async unMerge(arr, start, mid, end) {
+        this.color[start] = 1;
+        this.color[end] = 1;
+        const L = arr.slice(start, mid + 1);
+        const R = arr.slice(mid + 1, end + 1);
+        let k = start;
+        while (L.length && R.length) {
+            if (L[0] <= R[0]) {
+                arr[k] = L.shift();
+            } else {
+                arr[k] = R.shift();
+            }
+            this.color[k] = 2;
+            this.color[k - 1] = 0;
+            await this.sleep(5)
+            k++;
+
         }
-        for (let j = 0; j < n2; ++j) 
-        {
-            R.push(arr[mid+ 1 + j]);
+        while (L.length) {
+            arr[k] = L.shift();
+            this.color[k] = 2;
+            this.color[k - 1] = 0;
+            await this.sleep(5)
+            k++;
         }
-        let i = 0, j = 0; 
-        let k=start;
-        while (i<n1&&j<n2) {
-            while (i < n1 && j < n2) { 
-                //await this.sleep(100)
-                if (L[i] <= R[j]) { 
-                    arr[k] = L[i]; 
-                    await this.sleep(100)        
-                    i++; 
-                } 
-                else { 
-                    arr[k] = R[j]; 
-                    await this.sleep(100)
-                    j++; 
-                }                
-                k++;
-                await this.sleep(100) 
-                this.color[k]=2;
-                this.color[k-1]=0;
-            } 
-           
+        while (R.length) {
+            arr[k] = R.shift();
+            this.color[k] = 2;
+            this.color[k - 1] = 0;
+            await this.sleep(5)
+            k++;
+
         }
-        while (i < n1) { 
-            await this.sleep(100)
-            arr[k] = L[i]; 
-            i++; 
-            this.color[k]=2;
-            this.color[k-1]=0;
-            k++; 
-        } 
-        while (j < n2) { 
-            await this.sleep(100)
-            arr[k] = R[j]; 
-            j++; 
-            this.color[k]=2;
-            this.color[k-1]=0;
-            k++; 
-            
-        }
-        await this.sleep(100)
-        this.color[start]=0;
-        this.color[end]=0;
+        this.color[start] = 0;
+        this.color[end] = 0;
+        await this.sleep(5);
     }
-    //Vòng lặp
-    loop(){
-       this.draw();
-        setTimeout(()=>this.loop(),10)// cứ 10fps gọi lại hàm draw vẽ lại
+    loop() {
+            this.draw();
+            setTimeout(() => this.loop(), 10) // cứ 10fps gọi lại hàm draw vẽ lại
+        }
+        //xóa màn hình
+    clearScreen() {
+        this.context.fillStyle = "#000000"
+        this.context.fillRect(0, 0, 1200, 1000);
     }
-    //xóa màn hình
-    clearScreen()
-    {
-        this.context.fillStyle="#000000"
-        this.context.fillRect(0,0,1200,1000);
-    }
-     draw(){
+    draw() {
         this.clearScreen();
         for (let i = 0; i < this.arr.length; i++) {
             this.context.beginPath();
-            if(this.color[i]==1)
-                this.context.fillStyle ="#ff0000";
-            else if(this.color[i]==2)
-                this.context.fillStyle ="#ff00ff";
-            else this.context.fillStyle ="#ffffff";
-            this.context.fillRect(i*11,500,10,-this.arr[i]*5)
+            if (this.color[i] == 1)
+                this.context.fillStyle = "#ff0000";
+            else if (this.color[i] == 2)
+                this.context.fillStyle = "#ff00ff";
+            else this.context.fillStyle = "#ffffff";
+            this.context.fillRect(i * 1, 1000, 1, -this.arr[i])
             this.context.stroke();
         }
     }
-    sleep (time) {
+    sleep(time) {
         return new Promise((resolve) => setTimeout(resolve, time));
     }
 }
-   
-const Demo=new demo();
 
-
-
+const Demo = new demo();
--- test/index.js
+++ test/index.js
@@ -1,56 +1,28 @@
-const mergeSort=(arr)=>{
-    if(arr.length<=1)
+const mergeSort = (arr) => {
+    if (arr.length <= 1)
         return arr;
-    const right=[...arr];
-    const mid=Math.floor(arr.length/2);
-    const left=right.splice(0,mid);
-    return unMerge(mergeSort(left),mergeSort(right));
+    const right = [...arr];
+    const mid = Math.floor(arr.length / 2);
+    const left = right.splice(0, mid);
+    return unMerge(mergeSort(left), mergeSort(right));
 }
-const unMerge=(left,right)=>{
-    const newArr=[];
-    while (left.length&&right.length) {
-        if(left[0]<=right[0])
+const unMerge = (left, right) => {
+    const newArr = [];
+    while (left.length && right.length) {
+        if (left[0] <= right[0])
             newArr.push(left.shift());
         else newArr.push(right.shift());
     }
-    const result=[...newArr,...left,...right];
+    const result = [...newArr, ...left, ...right];
     return result;
 }
-// const arr= [2,5,6,1];
-// const mergeSort=(arr,start, end)=>{
-//     if(end>start)
-//     {
-//         const mid = Math.floor((start + end)/2);
-//         console.log(mid)
-//         mergeSort(arr, start, mid);//???
-//         mergeSort(arr, mid+1, end);//???
-//         console.log("me")
-//         unMerge(arr, start,mid, end);
-//         console.table(arr)
-//     }
-    
-// }
-// const unMerge=(arr, start,mid, end)=>{
-//    let i = start;
-//    let j = mid;
-//    while(i<=mid && j<=end)
-//    {
-//         if(arr[i] < arr[j])
-//         {
-//             swap(arr,i,j);   
-//         }
-//         i++;
-//         j++;
-//     }
-// }
-// const swap = (arr, a, b) => {
-//     const temp = arr[a];
-//     arr[a] = arr[b];
-//     arr[b] = temp;
-// }
-// console.time();
-// mergeSort(arr,0,arr.length);
-// console.log(arr);
-// console.timeEnd();
 
+const arr = [];
+for (let i = 0; i < 100; i++)
+    arr.push(i);
+const arrTest = arr.sort(() => Math.random() - 0.5)
 
+console.time();
+mergeSort(arrTest);
+console.timeEnd();
+console.log(arrTest)
