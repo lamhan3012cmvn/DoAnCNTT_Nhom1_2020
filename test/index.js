ddb7f6b341f1335334a0e287dd530bb40c688723
lamhan3012 <lamhan3012@gmail.com>
2020-10-28 00:46:37 -0700

    fixcode

--- test/index.js
+++ test/index.js
@@ -1,57 +1,29 @@
-// const mergeSort=(arr)=>{
-//     if(arr.length<=1)
-//         return arr;
-//     const right=[...arr];
-//     const mid=Math.floor(arr.length/2);
-//     const left=right.splice(0,mid);
-//     console.log(left,right);
-//     return unMerge(mergeSort(left),mergeSort(right));
-// }
-// const unMerge=(left,right)=>{
-//     const newArr=[];
-//     while (left.length&&right.length) {
-//         if(left[0]<=right[0])
-//             newArr.push(left.shift());
-//         else newArr.push(right.shift());
-//     }
-//     const result=[...newArr,...left,...right];
-//     return result;
-// }
-const arr= [2,5,6,1];
-const mergeSort=(arr,start, end)=>{
-    if(end>start)
-    {
-        const mid = Math.floor((start + end)/2);
-        console.log(mid)
-        mergeSort(arr, start, mid);//???
-        mergeSort(arr, mid+1, end);//???
-        console.log("me")
-        unMerge(arr, start,mid, end);
-        console.table(arr)
+const mergeSort=(arr)=>{
+    if(arr.length<=1)
+        return arr;
+    const right=[...arr];
+    const mid=Math.floor(arr.length/2);
+    const left=right.splice(0,mid);
+    return unMerge(mergeSort(left),mergeSort(right));
+}
+const unMerge=(left,right)=>{
+    const newArr=[];
+    while (left.length&&right.length) {
+        if(left[0]<=right[0])
+            newArr.push(left.shift());
+        else newArr.push(right.shift());
     }
-    
+    const result=[...newArr,...left,...right];
+    return result;
 }
-const unMerge=(arr, start,mid, end)=>{
-   let i = start;
-   let j = mid;
-   while(i<=mid && j<=end)
-   {
-        if(arr[i] < arr[j])
-        {
-            swap(arr,i,j);   
-        }
-        i++;
-        j++;
-    }
-}
-const swap = (arr, a, b) => {
-    const temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-}
+const arr= [];
+for(let i=0;i<1000000;i++)
+    arr.push(i);
+console.log("add done")
+arr.sort(()=>Math.random()-0.5)
+console.log("sort Done")
 console.time();
-mergeSort(arr,0,arr.length);
-console.log(arr);
+console.log(mergeSort(arr));
 console.timeEnd();
 
 
